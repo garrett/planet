5c6077e7c06de41939c8106af1488a278ac71e09
Jeff Waugh <jdub@perkypants.org>
2006-02-04 07:32:33 +0000

    further preparation for 1.0, fixing up distutils foo

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 """The Planet aggregator."""
 
-__authors__ = [ "Scott James Remnant <scott@netsplit.com>",
-                "Jeff Waugh <jdub@perkypants.org>" ]
-__license__ = "Python"
-
-
 from distutils.core import setup
 
-from planet import __version__ as planet_ver
+from planet import __version__ as VERSION
+from planet import __license__ as LICENSE
 
 setup(name="planet",
-      version=planet_ver,
+      version=VERSION,
+      description="The Planet Feed Aggregator",
+      author="Planet Developers",
+      author_email="devel@lists.planetplanet.org",
+      url="http://www.planetplanet.org/",
+      license=LICENSE,
       packages=["planet", "planet.compat_logging"],
       scripts=["planet.py", "planet-cache.py"],
-      description="The Planet aggregator",
-      url="http://www.planetplanet.org/",
-      author="Scott James Remnant and Jeff Waugh",
-      author_email="devel@lists.planetplanet.org",
       )
